33a3390d9df23153b23a17614d26acb716eff858
NerdyNarayan <nerdynarayan@gmail.com>
2024-07-29 21:28:21 +0530

    fix: deployment

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,33 +1,35 @@
-import { IconType } from "react-icons"
+import type { IconType } from "react-icons";
+
 import { ImBlog } from "react-icons/im";
 import { FaCode } from "react-icons/fa";
 import { RiRoadMapLine } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
-export type  NavItem={
-    title:string,
-    href:string,
-    icon:IconType
-}
-const blog:NavItem={
-    title:"blog",
-    href:"/blog",
-    icon:ImBlog
-}
-const project:NavItem={
-    title:"Projects",
-    href:"/projects",
-    icon:FaCode
-}
-const guide:NavItem={
-    title:"Guides",
-    href:"/guides",
-    icon:RiRoadMapLine
-}
-const about:NavItem={
-    title:"About",
-    href:"/about",
-    icon:FaUser
-}
-export const navItems={
-  items: [blog, project, guide, about] as NavItem[]
-}
+export type NavItem = {
+  title: string;
+  href: string;
+  icon: IconType;
+};
+const blog: NavItem = {
+  title: "blog",
+  href: "/blog",
+  icon: ImBlog,
+};
+const project: NavItem = {
+  title: "Projects",
+  href: "/projects",
+  icon: FaCode,
+};
+const guide: NavItem = {
+  title: "Guides",
+  href: "https://nerd-docs.vercel.app/",
+  icon: RiRoadMapLine,
+};
+const about: NavItem = {
+  title: "About",
+  href: "/about",
+  icon: FaUser,
+};
+
+export const navItems = {
+  items: [blog, project, guide, about] as NavItem[],
+};
